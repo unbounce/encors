788da1532d8077372b5ec4f1b7d94b741ac23632
Roman Gonzalez <roman@unbounce.com>
2014-11-21 12:00:04 -0800

    Add CorsPolicy type and smart constructor

---
+++ src/com/unbounce/encors/types.clj
@@ -0,0 +1,38 @@
+(ns com.unbounce.encors.types
+  (:require [schema.core :as s]))
+
+
+(deftype CorsPolicy
+    [allowed-origins
+     allowed-methods
+     request-headers
+     exposed-headers
+     max-age
+     allow-credentials?
+     origin-varies?
+     ignore-failures?])
+
+(def CorsPolicySchema
+  {(s/required-key :allowed-origins)    (s/maybe [s/Str])
+   (s/required-key :allowed-methods)    [(s/enum [:get :post :put :options])]
+   (s/required-key :request-headers)    [s/Str]
+   (s/required-key :exposed-headers)    (s/maybe [s/Str])
+   (s/required-key :max-age)            (s/maybe s/Int)
+   (s/required-key :allow-credentials?) s/Bool
+   (s/required-key :origin-varies?)     s/Bool
+   (s/required-key :ignore-failures?)   s/Bool})
+
+
+(defn create-cors-policy [& args-list]
+  (let [args (merge {:allow-credentials? true
+                     :ignore-failures? false}
+                    (apply hash-map args-list))]
+    (s/validate CorsPolicySchema args)
+    (CorsPolicy. (:allowed-origins args)
+                 (:allowed-methods args)
+                 (:request-headers args)
+                 (:exposed-headers args)
+                 (:max-age args)
+                 (:allow-credentials? args)
+                 (:origin-varies? args)
+                 (:ignore-failures? args))))
