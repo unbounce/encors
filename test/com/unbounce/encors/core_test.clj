12314cc94a49881576676561c0b1b3b220050005
David Dossot <david@dossot.net>
2014-11-21 12:12:11 -0800

    added jetty bootstrap to tests

--- test/com/unbounce/encors/core_test.clj
+++ test/com/unbounce/encors/core_test.clj
@@ -1,7 +1,27 @@
 (ns com.unbounce.encors.core-test
-  (:require [clojure.test :refer :all]))
+  (:require [clojure.test :refer :all]
+            [clj-http.client :as http]
+            [ring.adapter.jetty :refer [run-jetty]]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def ^:dynamic sut (atom nil))
 
+(def http-port
+  (let [configured-port
+        (or (System/getProperty "test.http.port")
+            (System/getenv "TEST_HTTP_PORT"))]
+    (if configured-port
+      (read-string configured-port)
+      (+ 50000 (rand-int 15000)))))
+
+(defn- handler [request]
+  (@sut request))
+
+(defn- with-server [f]
+  (let [jetty (run-jetty handler {:join? false :port http-port})]
+    (f)
+    (.stop jetty)))
+
+(use-fixtures :once with-server)
+
+(deftest no-cors-policy
+  (is true))
