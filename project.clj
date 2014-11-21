c44008a45f9979508208f809461f570bd7c26735
David Dossot <david@dossot.net>
2014-11-21 11:05:31 -0800

    added clj-http to dev deps, added linters to dev plugins

--- project.clj
+++ project.clj
@@ -4,4 +4,8 @@
   :license {:name "The MIT License (MIT)"
             :url "http://opensource.org/licenses/MIT"
             :comments "Copyright (c) 2014 Unbounce Marketing Solutions Inc."}
+  
+  :profiles {:dev {:plugins [[lein-kibit "0.0.8"][jonase/eastwood "0.1.4"]] 
+                   :dependencies [[clj-http "1.0.1"]]}}
+
   :dependencies [[org.clojure/clojure "1.6.0"]])
