4f063a19b7bdbbe2cce70c747f2adb33b20d77ad
David Dossot <david@dossot.net>
2014-11-21 11:31:27 -0800

    added ring deps

--- project.clj
+++ project.clj
@@ -1,3 +1,5 @@
+(def ring-version "1.3.1")
+
 (defproject com.unbounce/encors "0.1.0-SNAPSHOT"
   :description "encors is a CORS library for ring"
   :url "https://www.github.com/unbounce/encors"
@@ -5,8 +7,10 @@
             :url "http://opensource.org/licenses/MIT"
             :comments "Copyright (c) 2014 Unbounce Marketing Solutions Inc."}
   
-  :profiles {:dev {:plugins [[lein-kibit "0.0.8"][jonase/eastwood "0.1.4"]] 
-                   :dependencies [[clj-http "1.0.1"]]}}
+  :profiles {:dev {:plugins [[lein-kibit "0.0.8"]
+                             [jonase/eastwood "0.1.4"]] 
+                   :dependencies [[clj-http "1.0.1"]
+                                  [ring/ring-jetty-adapter ~ring-version]]}}
 
-  :dependencies [[org.clojure/clojure "1.6.0"]])
-
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [ring/ring-core ~ring-version]])
