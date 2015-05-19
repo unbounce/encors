b16563b8c1c666ccc2ce8c4051619b2dc748cb8b
David Dossot <david@dossot.net>
2015-05-19 08:51:40 -0700

    inced many deps

--- project.clj
+++ project.clj
@@ -9,9 +9,9 @@
 
   :profiles {:dev {:plugins [[lein-kibit "0.0.8"]
                              [jonase/eastwood "0.2.1"]]
-                   :dependencies [[clj-http "1.0.1"]
+                   :dependencies [[clj-http "1.1.2"]
                                   [ring/ring-jetty-adapter ~ring-version]
-                                  [compojure "1.3.2"]]}}
+                                  [compojure "1.3.4"]]}}
 
   ; set to true to have verbose debug of integration tests
   :jvm-opts ["-Ddebug=false"]
@@ -19,7 +19,7 @@
   :eastwood {:exclude-namespaces [com.unbounce.encors]}
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [potemkin "0.3.12"]
-                 [org.clojure/core.match "0.2.1"]
-                 [prismatic/schema "0.4.0"]
+                 [potemkin "0.3.13"]
+                 [org.clojure/core.match "0.2.2"]
+                 [prismatic/schema "0.4.2"]
                  [ring/ring-core ~ring-version]])
