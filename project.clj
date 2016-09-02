f7739d2222a3aef9ac72675ef79765fc6b7efbf6
Raymond Huang <rymndhng@gmail.com>
2016-09-01 22:05:25 -0700

    Bump year and deps to support Clojure 1.8
    Because it's year 2016!

--- project.clj
+++ project.clj
@@ -1,25 +1,26 @@
-(def ring-version "1.3.2")
+(def ring-version "1.4.0")
 
 (defproject com.unbounce/encors "2.3.0-SNAPSHOT"
   :description "encors is a CORS library for ring"
   :url "https://www.github.com/unbounce/encors"
   :license {:name "The MIT License (MIT)"
             :url "http://opensource.org/licenses/MIT"
-            :comments "Copyright (c) 2014-2015 Unbounce Marketing Solutions Inc."}
+            :comments "Copyright (c) 2014-2016 Unbounce Marketing Solutions Inc."}
 
-  :profiles {:dev {:plugins [[lein-kibit "0.0.8"]
-                             [jonase/eastwood "0.2.1"]]
-                   :dependencies [[clj-http "1.1.2"]
+  :profiles {:dev {:plugins [[lein-kibit "0.1.2"]
+                             [jonase/eastwood "0.2.3"]]
+                   :dependencies [[clj-http "3.2.0"]
                                   [ring/ring-jetty-adapter ~ring-version]
-                                  [compojure "1.3.4"]]}}
+                                  [compojure "1.4.0"]
+                                  [manifold "0.1.5"]]}}
 
   ; set to true to have verbose debug of integration tests
   :jvm-opts ["-Ddebug=false"]
 
   :eastwood {:exclude-namespaces [com.unbounce.encors]}
 
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [potemkin "0.3.13"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [potemkin "0.4.3"]
                  [org.clojure/core.match "0.2.2"]
-                 [prismatic/schema "0.4.3"]
+                 [prismatic/schema "1.1.3"]
                  [ring/ring-core ~ring-version]])
